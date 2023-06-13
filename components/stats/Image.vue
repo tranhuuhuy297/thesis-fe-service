f1c01d82aa92afb4089da5a4bbad930452e0607c
tranhuuhuy297 <tranhuuhuy297@gmail.com>
2023-06-14 00:56:58 +0700

    add upvote

--- components/stats/Image.vue
+++ components/stats/Image.vue
@@ -5,93 +5,69 @@
       cover
       class="pointer mb-2 img"
     />
-    <div
-      style="
-        position: absolute;
-        bottom: 4px;
-        left: 4px;
-        font-size: 12px;
-        max-width: 100%;
-      "
-      class="font-weight-bold text-white text-truncate"
-    >
-      {{ prompt?.user_gmail ? `@${prompt?.user_gmail}` : "" }}
-    </div>
+    <v-btn
+      :icon="isUpvote ? 'mdi-heart' : 'mdi-heart-broken'"
+      :color="isUpvote ? 'error' : 'white'"
+      style="position: absolute; top: 2px; right: 2px"
+      variant="text"
+      @click.stop="handleUpvote"
+    ></v-btn>
   </div>
-  <v-dialog v-model.trim="isShowDialog" persistent>
-    <v-card class="bg-bg-1 mx-auto" max-width="50vw" max-height="100vh">
-      <v-card-title class="d-flex justify-end">
-        <v-btn icon="mdi-close" variant="text" @click="isShowDialog = false">
-        </v-btn>
-      </v-card-title>
-      <v-card-text class="d-flex mt-0 pt-0 mb-2">
-        <div
-          class="rounded-lg w-50 d-flex align-center justify-center"
-          style="position: relative"
-        >
-          <v-img :src="`https://${prompt?.image_src}`" cover />
+  <v-dialog v-model.trim="isShowDialog" width="auto" persistent>
+    <v-card>
+      <v-card-text>
+        <div class="text-h6 font-weight-bold d-flex al">
+          <span class="text-info">Prompt Detail</span>
+          <span class="mx-2">-</span>
+          <span class="font-italic font-weight-medium" style="font-size: 14px">
+            @{{ prompt.user_gmail }}
+          </span>
         </div>
-        <v-divider vertical class="mx-4 border-opacity-50" :thickness="2">
-        </v-divider>
-        <div class="flex-grow-1">
-          <div v-if="!isShowDelete">
-            <v-btn
-              prepend-icon="mdi-heart"
-              color="red"
-              :text="count"
-              :variant="isUpvote ? 'flat' : 'outlined'"
-              class="mb-2"
-              @click="handleUpvote"
-            ></v-btn>
-          </div>
-          <div class="text-h5">Prompt</div>
+        <div class="mt-4 d-flex">
           <v-textarea
-            :model-value="prompt?.prompt_content"
-            variant="outlined"
-            hide-details
-            placeholder="Type your prompt here..."
             bg-color="bg-1"
-            class="mt-1"
-            :readonly="!isShowDelete"
-          />
-          <div class="text-h5 mt-4">Negative Prompt</div>
-          <v-textarea
-            :model-value="prompt?.negative_prompt"
+            label="Prompt"
+            :model-value="prompt.prompt"
             variant="outlined"
-            hide-details
-            :placeholder="
-              !isShowDelete ? '' : 'Type your negative prompt here...'
-            "
-            bg-color="bg-1"
-            class="mt-1"
-            :readonly="!isShowDelete"
-          />
-          <div class="text-h5 mt-4">Model</div>
-          <v-autocomplete
-            :model-value="prompt?.model"
-            :items="['midjourney', 'stable-diffustion']"
-            variant="outlined"
-            hide-details
-            class="mt-1"
-            bg-color="bg-1"
+            class="mx-2"
+            auto-grow
             readonly
-          ></v-autocomplete>
+          >
+            <template #append-inner>
+              <v-btn
+                prepend-icon="mdi-content-copy"
+                color="success"
+                text="Copy"
+                class="text-none text-text-1"
+                variant="flat"
+                @click="handleCopyPrompt"
+              ></v-btn>
+            </template>
+          </v-textarea>
+          <v-textarea
+            bg-color="bg-1"
+            label="Negative Prompt"
+            :model-value="prompt.negative_prompt"
+            variant="outlined"
+            class="mx-2"
+            auto-grow
+            readonly
+          >
+            <template #append-inner>
+              <v-btn
+                prepend-icon="mdi-content-copy"
+                color="success"
+                text="Copy"
+                class="text-none text-text-1"
+                variant="flat"
+                @click="handleCopyNegPrompt"
+              ></v-btn>
+            </template>
+          </v-textarea>
         </div>
       </v-card-text>
-      <v-card-actions v-if="isShowDelete" class="d-flex justify-end">
-        <v-btn
-          variant="text"
-          text="Close"
-          size="large"
-          @click="isShowDialog = false"
-        ></v-btn>
-        <v-btn
-          variant="flat"
-          color="error"
-          text="Delete"
-          size="large"
-          @click="isShowDeleteDialog = true"
-        ></v-btn>
+      <v-card-actions class="d-flex justify-end">
+        <v-btn variant="text" @click="isShowDialog = false">Close</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -113,8 +89,8 @@
         >
           Delete
         </v-btn>
-      </v-card-actions></v-card
-    >
+      </v-card-actions>
+    </v-card>
   </v-dialog>
 </template>
 
@@ -135,90 +111,43 @@ const isShowDialog = ref(false);
 const isShowDeleteDialog = ref(false);
 const isLoadingDelete = ref(false);
 
-async function handleDeletePrompt() {
-  isLoadingDelete.value = true;
-  const { data } = await useFetch(`${baseURL}/prompt/${props?.prompt.id}`, {
-    method: "DELETE",
-    headers: {
-      Authorization: `Bearer ${getCookie("token")}`,
-    },
-  });
-  isLoadingDelete.value = false;
-  if (!data.value) return;
-  const { result, code, msg } = data.value;
-  if (code === CODE_SUCCESS) {
-    useNuxtApp().$toast.success("Delete successfully!");
-    isShowDeleteDialog.value = false;
-    setTimeout(() => {
-      isShowDialog.value = false;
-    }, 200);
-    emit("deletedPrompt");
-  }
-}
+async function handleDeletePrompt() {}
 const userStore = useUserStore();
-async function handleUpvote() {
-  if (isUpvote.value) {
-    handleDownUpvote();
-    return;
-  }
-  const { data } = await useFetch(`${baseURL}/upvote`, {
-    method: "POST",
-    body: {
-      user_id: userStore.id,
-      prompt_id: props.prompt.id,
-    },
-    headers: {
-      Authorization: `Bearer ${getCookie("token")}`,
-    },
-  });
-  if (!data.value) return;
-  const { result, code, msg } = data.value;
-  handleGetUpvote();
-  if (code === CODE_SUCCESS) {
-    useNuxtApp().$toast.success("Upvote successfully!");
-  }
-}
-
-async function handleDownUpvote() {
-  const { data } = await useFetch(`${baseURL}/upvote/${upvote.value?.id}`, {
-    method: "DELETE",
-    headers: {
-      Authorization: `Bearer ${getCookie("token")}`,
-    },
-  });
-  if (!data.value) return;
-  const { result, code, msg } = data.value;
-  handleGetUpvote();
-  if (code === CODE_SUCCESS) {
-    useNuxtApp().$toast.success("Downvote successfully!");
-  }
-}
 
 watch(
   () => isShowDialog.value,
-  () => {
-    handleGetUpvote();
-  }
+  () => {}
 );
 
+function handleCopyPrompt() {
+  navigator.clipboard.writeText(props.prompt.prompt);
+  useNuxtApp().$toast.success("Copy to clipboard!");
+}
+
+function handleCopyNegPrompt() {
+  navigator.clipboard.writeText(props.prompt.negative_prompt);
+  useNuxtApp().$toast.success("Copy to clipboard!");
+}
+
 const isUpvote = ref(false);
-const count = ref(0);
-const upvote = ref(null);
-async function handleGetUpvote() {
-  const { data } = await useFetch(
-    `${baseURL}/upvote/user/${userStore.id}/prompt/${props.prompt.id}`,
-    {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${getCookie("token")}`,
-      },
-    }
-  );
-  if (!data.value) return;
-  const { result, code, msg } = data.value;
-  isUpvote.value = result.isUpvote;
-  count.value = result.count;
-  upvote.value = result.upvote;
+
+onMounted(() => {
+  nextTick(() => (isUpvote.value = props.prompt.is_upvote));
+});
+
+function handleUpvote() {
+  const { data } = useFetch(`${baseURL}/upvote`, {
+    method: isUpvote.value ? "DELETE" : "POST",
+    body: {
+      user_id: userStore.id,
+      prompt_id: props.prompt.prompt_id,
+    },
+    headers: {
+      Authorization: `Bearer ${getCookie("token")}`,
+    },
+  });
+  isUpvote.value = !isUpvote.value;
+  useNuxtApp().$toast.success("Successfully!");
 }
 </script>
 
--- pages/stats.vue
+++ pages/stats.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-h3 font-weight-bold">
     Prompt <span class="text-primary-2">Stats</span>
-    <div class="mt-2 text-h6">Search for the best prompts</div>
+    <div class="mt-2 text-h6">Search for the best images</div>
   </div>
   <div class="mt-8 text-h5">
     <v-tabs v-model.trim="currentTab" color="text-1">
@@ -15,7 +15,7 @@
     <v-divider style="margin-top: 1px"></v-divider>
     <v-window v-model.trim="currentTab" class="flex-grow-1 mt-4">
       <div v-if="currentTab === 1" class="d-flex">
-        <div class="mx-1" style="width: 20%">
+        <div class="mx-1 w-25">
           <div
             v-for="(prompt, index) in newestPromptCol0"
             :key="`${index}_prompt_col0`"
@@ -23,7 +23,7 @@
             <StatsImage :prompt="prompt"></StatsImage>
           </div>
         </div>
-        <div class="mx-1" style="width: 20%">
+        <div class="mx-1 w-25">
           <div
             v-for="(prompt, index) in newestPromptCol1"
             :key="`${index}_prompt_col1`"
@@ -31,7 +31,7 @@
             <StatsImage :prompt="prompt"></StatsImage>
           </div>
         </div>
-        <div class="mx-1" style="width: 20%">
+        <div class="mx-1 w-25">
           <div
             v-for="(prompt, index) in newestPromptCol2"
             :key="`${index}_prompt_col2`"
@@ -39,18 +39,10 @@
             <StatsImage :prompt="prompt"></StatsImage>
           </div>
         </div>
-        <div class="mx-1" style="width: 20%">
+        <div class="mx-1 w-25">
           <div
             v-for="(prompt, index) in newestPromptCol3"
             :key="`${index}_prompt_col3`"
-          >
-            <StatsImage :prompt="prompt"></StatsImage>
-          </div>
-        </div>
-        <div class="mx-1" style="width: 20%">
-          <div
-            v-for="(prompt, index) in newestPromptCol4"
-            :key="`${index}_prompt_col4`"
           >
             <StatsImage :prompt="prompt"></StatsImage>
           </div>
@@ -65,7 +57,7 @@ import { usePromptStore } from "~/stores/Prompt";
 
 const currentTab = ref(1);
 const config = useRuntimeConfig();
-const baseURL = `${config.public.baseURL}/prompt`;
+const baseURL = `${config.public.baseURL}`;
 
 const promptStore = usePromptStore();
 
@@ -89,7 +81,7 @@ onMounted(() => {
 });
 
 async function handleGetListPrompt() {
-  const { data, pending } = await useLazyFetch(`${baseURL}`, {
+  const { data, pending } = await useLazyFetch(`${baseURL}/image`, {
     method: "GET",
     query: textSearch.value
       ? { page: page.value, size: size.value, search: textSearch.value }
@@ -111,35 +103,28 @@ async function handleGetListPrompt() {
 
 const newestPromptCol0 = computed(() => {
   return promptStore.newestListPrompt.filter((prompt, index) => {
-    if (index % 5 === 0) {
+    if (index % 4 === 0) {
       return prompt;
     }
   });
 });
 const newestPromptCol1 = computed(() => {
   return promptStore.newestListPrompt.filter((prompt, index) => {
-    if (index % 5 === 1) {
+    if (index % 4 === 1) {
       return prompt;
     }
   });
 });
 const newestPromptCol2 = computed(() => {
   return promptStore.newestListPrompt.filter((prompt, index) => {
-    if (index % 5 === 2) {
+    if (index % 4 === 2) {
       return prompt;
     }
   });
 });
 const newestPromptCol3 = computed(() => {
   return promptStore.newestListPrompt.filter((prompt, index) => {
-    if (index % 5 === 3) {
-      return prompt;
-    }
-  });
-});
-const newestPromptCol4 = computed(() => {
-  return promptStore.newestListPrompt.filter((prompt, index) => {
-    if (index % 5 === 4) {
+    if (index % 4 === 3) {
       return prompt;
     }
   });
